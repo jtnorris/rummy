e15333f5c42d19b2d52529989870a41244f031e7
John Norris <john.t.norris@bt.com>
2012-11-23 18:05:42 +0000

    more awesomeness

--- public/js/users.js
+++ public/js/users.js
@@ -2,6 +2,7 @@ $.rummy = {
   player: {},
 
   game: {},
+  games: [],
   sharks: [],
   shark: {},
 
@@ -15,6 +16,13 @@ $.rummy = {
     } else {
       $.rummy.loadPage('games');
     }
+  },
+  addGame: function () {
+    dpd.games.post({"nextToGo": $.rummy.player.id}, function(game) {
+      $.pub('game/add', {
+        game: game
+      });
+    });
   },
   getGames: function () {
     var query = {};
@@ -36,10 +44,27 @@ $.rummy = {
     });
   },
 
+  getPlayersGames: function () {
+    dpd.sharks.get({"playerId": $.rummy.player.id}, function (sharks) {
+      var i = sharks.length;
+      while (i--) {
+        $.rummy.games.push(sharks[i].gameId);
+      }
+      $.pub('player/games', {
+        'games': $.rummy.games
+      });
+    });
+  },
+
   getShark: function () {
     dpd.sharks.get({"gameId": $.rummy.game.id, "playerId": $.rummy.player.id}, function (shark) {
-      $.rummy.shark = shark;
+      $.rummy.shark = shark[0];
       $.pub('shark/get');
+    });
+  },
+  addShark: function (data) {
+    dpd.sharks.post(data, function(shark) {
+      $.pub('shark/add');
     });
   },
   setShark: function (data) {
@@ -62,16 +87,30 @@ $(document).ready(function () {
 
   $.sub('player/get', function () {
     $.rummy.getGame();
-    $.sub('game/get', function () {
-      $.rummy.loadPage('game');
-      $.sub('page/game', function () {
-        $.rummy.getShark();
-      });
+  });
+
+  $.sub('game/get', function () {
+    $.rummy.loadPage('game');
+  });
+
+  $.sub('game/add', function (e, args) {
+    $.rummy.addShark({
+      'gameId': args.game.id,
+      'playerId': $.rummy.player.id
     });
+  });
+
+  $.sub('shark/add', function () {
+    window.location = window.location;
+  });
+
+  $.sub('page/game', function () {
+    $.rummy.getShark();
   });
 
   $.sub('page/games', function () {
     $.rummy.getGames();
+    $.rummy.getPlayersGames();
   });
 
 });
--- public/js/views.js
+++ public/js/views.js
@@ -4,6 +4,33 @@ $.sub('games/get', function (e, args) {
   });
 });
 
+$.sub('player/games', function (e, args) {
+  $.each(args.games, function (i, n) {
+    $('#your-games').append('<li><a href="/?game=' + n + '">' + n + '</a></li>');
+  });
+});
+
+$.sub('page/game', function () {
+  var cards = $.rummy.game.deck.length,
+    i = 0,
+    $card;
+  while (i < cards) {
+    $card = $('<article></article>').css({
+      'top': (i/3) +'px',
+      'left': (i/3) +'px'
+    });
+    $('#deck').append($card);
+    i++;
+  }
+});
+
+$.sub('shark/get', function () {
+  $.each($.rummy.shark.hand, function (i, n) {
+    $('#hand').append(cardTemplate(n.score).replace(/\{{card}}/g, n.card).replace(/\{{suit}}/g, n.suit).replace(/\{{rank}}/g, n.rank).replace(/\{{symbol}}/g, n.symbol));
+  });
+  var baraja = $('#hand').baraja();
+});
+
 $.sub('page/login', function () {
   $('button' ,'#login_form').click(function (e) {
       e.preventDefault();
@@ -30,3 +57,29 @@ $.sub('page/login', function () {
         }
     });
 });
+
+$.sub('page/games', function () {
+  $('#add-game').click(function () {
+    $.rummy.addGame();
+  });
+});
+
+cardTemplate = function (score) {
+  var templated = '<article class="card {{suit}}">' +
+    '<h1>{{rank}}</h1>' +
+    '<ul class="symbols">';
+      if (score <= 10) {
+        for (var i=0; i<score; i++) {
+          templated += '<li>{{symbol}}</li>'
+        }
+      }
+    templated += '</ul>' +
+    '<span class="header">' +
+      '{{rank}} {{symbol}} <h2>{{rank}} <em>of</em> {{suit}}</h2>' +
+    '</span>' +
+    '<span class="footer">' +
+      '{{rank}} {{symbol}} <h2>{{rank}} <em>of</em> {{suit}}</h2>' +
+    '</span>' +
+  '</article>';
+  return templated;
+};
